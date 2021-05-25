f909d5df120008751a395ff64468edee65fef1d8
hyemmie <lhm0710@naver.com>
2021-05-25 10:49:33 +0900

    [add] type and interface

---
+++ src/types/propertyType.ts
@@ -0,0 +1,10 @@
+export type propertyType = {
+  imageUrl: string;
+  imageAlt: string;
+  type: string;
+  start: string;
+  title: string;
+  totalAmount: string;
+  goal: string;
+  contributors: number;
+};
