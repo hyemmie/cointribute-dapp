379cb14432f27c6e11dba2a88b7640687abbbc4f
hyemmie <lhm0710@naver.com>
2021-06-01 19:29:38 +0900

    refactor function component

--- src/components/BeneficiaryCard.tsx
+++ src/components/BeneficiaryCard.tsx
@@ -1,57 +1,72 @@
 import React from "react";
-import { Box, Badge, Image } from "@chakra-ui/react";
+import { Box, Badge, Image, Link } from "@chakra-ui/react";
+import { Link as ReachLink } from "react-router-dom";
+
 import { propertyType } from "../types/propertyType";
 
-export function BeneficiaryCard(props: propertyType) {
-  return (
-    <Box
-      maxW="sm"
-      borderWidth="1px"
-      borderRadius="lg"
-      overflow="hidden"
-      style={{ margin: 10, boxShadow: "0px 0.5px 6px #00000020" }}
-    >
-      <Image src={props.imageUrl} alt={props.imageAlt} />
-      <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          <Badge
-            borderRadius="full"
-            px="2"
-            colorScheme={props.type === "Individual" ? "teal" : "blue"}
-          >
-            {props.type}
-          </Badge>
-          <Box
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml="2"
-          >
-            from {props.start}
-          </Box>
-        </Box>
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          {props.title}
-        </Box>
-        <Box>
-          {props.totalAmount}
-          <Box as="span" color="gray.600" fontSize="sm">
-            / {props.goal}
-          </Box>
-        </Box>
+type Props = {
+  beneficiary: propertyType;
+};
 
-        <Box as="span" ml="2" color="gray.600" fontSize="sm">
-          {props.contributors} contributors
+export const BeneficiaryCard: React.FunctionComponent<Props> = (
+  props: Props
+) => {
+  return (
+    <Link as={ReachLink} to={`/beneficiary/${props.beneficiary.id}`}>
+      <Box
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        style={{ margin: 10, boxShadow: "0px 0.5px 6px #00000020" }}
+      >
+        <Image
+          src={props.beneficiary.imageUrl}
+          alt={props.beneficiary.imageAlt}
+        />
+        <Box p="6">
+          <Box d="flex" alignItems="baseline">
+            <Badge
+              borderRadius="full"
+              px="2"
+              colorScheme={
+                props.beneficiary.type === "Individual" ? "teal" : "blue"
+              }
+            >
+              {props.beneficiary.type}
+            </Badge>
+            <Box
+              color="gray.500"
+              fontWeight="semibold"
+              letterSpacing="wide"
+              fontSize="xs"
+              textTransform="uppercase"
+              ml="2"
+            >
+              from {props.beneficiary.start}
+            </Box>
+          </Box>
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            {props.beneficiary.title}
+          </Box>
+          <Box>
+            {props.beneficiary.totalAmount}
+            <Box as="span" color="gray.600" fontSize="sm">
+              / {props.beneficiary.goal}
+            </Box>
+          </Box>
+
+          <Box as="span" ml="2" color="gray.600" fontSize="sm">
+            {props.beneficiary.contributors} contributors
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </Link>
   );
-}
+};
