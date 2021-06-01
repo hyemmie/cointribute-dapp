ffe559796b8fa3a9a9e177cb1d9998b317a1a2b4
hyemmie <lhm0710@naver.com>
2021-06-01 19:29:16 +0900

    Refactor provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Main from "./containers/Main";
+import Donate from "./containers/Donate";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import getLibrary from "./core/utils/getLibrary";
 
 function App() {
-  return (
-    <ChakraProvider>
-      <Router>
-        <Switch>
-          <Route path="/main">
-            <Main />
-          </Route>
-          <Route path="/beneficiary/:id"></Route>
-        </Switch>
-      </Router>
-    </ChakraProvider>
-  );
+  if (window.ethereum?.isMetaMask) {
+    return (
+      <ChakraProvider>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <Router>
+            <Switch>
+              <Route path="/main">
+                <Main />
+              </Route>
+              <Route path="/beneficiary/:id">
+                <Donate />
+              </Route>
+            </Switch>
+          </Router>
+        </Web3ReactProvider>
+      </ChakraProvider>
+    );
+  } else {
+    return <div>Install Metamask</div>;
+  }
 }
 
 export default App;
--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-// import { useHistory, useParams } from "react-router-dom";
-import { Web3ReactProvider } from "@web3-react/core";
-import getLibrary from "../core/utils/getLibrary";
 import Index from "../components/Index";
 
 function Main() {
-  if (window.ethereum?.isMetaMask) {
-    return (
-      <div>
-        <Web3ReactProvider getLibrary={getLibrary}>
-          <Index />
-        </Web3ReactProvider>
-      </div>
-    );
-  } else {
-    return <div>Install Metamask</div>;
-  }
+  return <Index />;
 }
 
 export default Main;
