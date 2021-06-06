486b71e7c7ce8c653d733f4842d051c64ae35619
hyemmie <lhm0710@naver.com>
2021-06-07 02:33:50 +0900

    add property 2

--- src/beneficiary/property.ts
+++ src/beneficiary/property.ts
@@ -2,7 +2,7 @@ export const property = [
   {
     id: 0,
     imageUrl:
-      "https://cdn.pixabay.com/photo/2016/11/29/13/56/asian-1870022__480.jpg",
+      "https://cdn.pixabay.com/photo/2018/06/27/07/45/student-3500990__480.jpg",
     imageAlt: "Rear view of modern home with pool",
     type: "Individual",
     start: "2021.05.20",
@@ -14,11 +14,23 @@ export const property = [
   {
     id: 1,
     imageUrl:
-      "https://cdn.pixabay.com/photo/2015/07/17/22/42/startup-849804__480.jpg",
+      "https://cdn.pixabay.com/photo/2016/03/26/17/45/teacher-1280966__480.jpg",
     imageAlt: "Rear view of modern home with pool",
     type: "Organization",
     start: "2021.05.19",
     title: "Beneficiary 2",
+    totalAmount: "200.00 CTB",
+    goal: "500.00 CTB",
+    contributors: 50,
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2017/12/31/12/38/adult-3052244__480.jpg",
+    imageAlt: "Rear view of modern home with pool",
+    type: "Individual",
+    start: "2021.05.19",
+    title: "Beneficiary 3",
     totalAmount: "200.00 CTB",
     goal: "500.00 CTB",
     contributors: 50,
